608badeb13f1292edda8dd396d4aa3f6889d2717
sarpsahinalp <sarpsahinalp.official@gmail.com>
2025-11-25 20:44:39 +0100

    Add persistent objects and an example migration

--- backend/core/src/main/java/decidish/com/core/model/rewe/Market.java
+++ backend/core/src/main/java/decidish/com/core/model/rewe/Market.java
@@ -1,8 +1,22 @@
 package decidish.com.core.model.rewe;
 
-public record Market(
-        String id,
-        String name,
-        Address address,
-        boolean isOpen
-) {}
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "markets")
+@Data
+public class Market {
+    @Id
+    @SequenceGenerator(sequenceName = "market_sequence", name = "market_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "market_sequence")
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    boolean isOpen;
+}
---
+++ backend/core/src/main/migration/V1__init.sql
@@ -0,0 +1,25 @@
+CREATE SEQUENCE IF NOT EXISTS market_sequence START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE markets
+(
+    id         BIGINT  NOT NULL,
+    name       VARCHAR(255),
+    address_id BIGINT,
+    is_open    BOOLEAN NOT NULL,
+    CONSTRAINT pk_markets PRIMARY KEY (id)
+);
+
+ALTER TABLE markets
+    ADD CONSTRAINT uc_markets_address UNIQUE (address_id);
+
+ALTER TABLE markets
+    ADD CONSTRAINT FK_MARKETS_ON_ADDRESS FOREIGN KEY (address_id) REFERENCES addresses.address (id);
+
+CREATE TABLE addresses.address
+(
+    id       BIGINT NOT NULL,
+    street   VARCHAR(255),
+    zip_code VARCHAR(255),
+    city     VARCHAR(255),
+    CONSTRAINT pk_address PRIMARY KEY (id)
+);
