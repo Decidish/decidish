d6aa73dcfd5effeda51cf5af647de4e83bb79492
sarpsahinalp <sarpsahinalp.official@gmail.com>
2025-12-01 18:09:40 +0100

    Setup kafka broker and send event on like/dislike action

--- backend/personalization/config/app_config.go
+++ backend/personalization/config/app_config.go
@@ -6,22 +6,29 @@ import (
 )
 
 type ApplicationConfig struct {
-	JWTSecret       string
-	DBConnectionUrl string
+	JWTSecret          string
+	DBConnectionUrl    string
+	KafkaConnectionUrl string
 }
 
 func (config *ApplicationConfig) LoadConfiguration() {
 	config.JWTSecret = os.Getenv("JWT_SECRET")
 
 	if config.JWTSecret == "" {
-	    // throw an error or set a default value
+		// throw an error or set a default value
 		panic(errors.New("JWT_SECRET environment variable is required"))
 	}
 
 	config.DBConnectionUrl = os.Getenv("DATABASE_URL")
 
 	if config.DBConnectionUrl == "" {
-	    // throw an error or set a default value
+		// throw an error or set a default value
 		panic(errors.New("DATABASE_URL environment variable is required"))
+	}
+
+	config.KafkaConnectionUrl = os.Getenv("KAFKA_CONNECTION_URL")
+
+	if config.KafkaConnectionUrl == "" {
+		panic(errors.New("KAFKA_CONNECTION_URL environment variable is required"))
 	}
 }
--- backend/personalization/db/scripts/migrate_go.go
+++ backend/personalization/db/scripts/migrate_go.go
@@ -8,36 +8,11 @@ import (
 func ExecuteGoMigrations(db *sql.DB) error {
 	goVersions := []string{"20251130150754"}
 
-	var existingId string
-	err := db.QueryRow(
-		`SELECT version_id FROM goose_db_version WHERE version_id = $1`,
-		goVersions[0]).Scan(&existingId)
-	if err == nil {
-		// Version already applied, skip
-		return nil
-	}
-	if !errors.Is(err, sql.ErrNoRows) {
-		// Unexpected error
-		return err
-	}
-
 	// TODO: In the future we should consider multiple go versions
-	err = UpSeedRecipesTable(db)
+	err := UpSeedRecipesTable(db)
 	if err != nil {
 		return err
 	}
-
-	_, err = db.Exec(`
-	INSERT INTO decidish.public.goose_db_version (version_id, is_applied)
-	VALUES ($1, $2)	
-	`, goVersions[0], true)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
 
 	var existingId string
 	err = db.QueryRow(
