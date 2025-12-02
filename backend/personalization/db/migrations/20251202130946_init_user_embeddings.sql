3a618b0c91b967421b0c21121e8d8246803f6ed6
sarpsahinalp <sarpsahinalp.official@gmail.com>
2025-12-02 14:17:39 +0100

    Create user embeddings schema

---
+++ backend/personalization/db/migrations/20251202130946_init_user_embeddings.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE user_embeddings (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    embedding vector(10)
+);
+
+CREATE INDEX ON user_embeddings USING hnsw (embedding vector_cosine_ops)
+WITH (m = 16, ef_construction = 200);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+-- +goose StatementEnd
