9c3e7f66015be2ed82f0ab9751b2ecda5be38942
Elas234 <eliasmonge234@gmail.com>
2025-12-02 14:10:25 +0100

    Refactor ReweApiClient to better design, cleaned unused imports, cleaned unnecessary debug comments

--- backend/core/src/main/java/decidish/com/core/api/rewe/client/ReweApiClient.java
+++ backend/core/src/main/java/decidish/com/core/api/rewe/client/ReweApiClient.java
@@ -1,54 +1,46 @@
 package decidish.com.core.api.rewe.client;
 
-import decidish.com.core.model.rewe.MarketDto;
 import decidish.com.core.model.rewe.MarketSearchResponse;
 import decidish.com.core.model.rewe.MarketDetailsResponse;
 import decidish.com.core.model.rewe.ProductSearchResponse;
 
-import org.hibernate.validator.constraints.URL;
-import org.springframework.messaging.handler.annotation.Headers;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-import java.net.URI;
-
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
-import java.net.URI;
-
-/**
- * Maps to the Rewe Mobile API v3
- */
-// @HttpExchange("/api/v3")
 @HttpExchange
 public interface ReweApiClient {
 
-    /**
-     * url = "https://..." + "/api/v3/market/search?search=" + str(zip_code)
-     */
+    public static final String REWE_API_BASE_URL = "https://mobile-api.rewe.de/api/v3";
+    public static final String REWE_CLIENT_API_BASE_URL = "https://mobile-clients-api.rewe.de/api";
 
-    @GetExchange
+    public static final String MARKET_SEARCH_PATH = "/market/search";
+    public static final String MARKET_DETAILS_PATH = "/market/details";
+    public static final String PRODUCT_SEARCH_PATH = "/products";
+
+    // e.g.,
+    // https://mobile-api.rewe.de/api/v3/market/search?search=80995
+    @GetExchange(REWE_API_BASE_URL + MARKET_SEARCH_PATH)
     MarketSearchResponse searchMarkets(
-        URI uri,
         @RequestParam("search") String zipCode
     );
 
-    @GetExchange
+    // e.g.,
+    // https://mobile-api.rewe.de/api/v3/market/details?marketId=431022
+    @GetExchange(REWE_API_BASE_URL + MARKET_DETAILS_PATH)   
     MarketDetailsResponse getMarketDetails(
-        URI uri, 
         @RequestParam("marketId") String marketId
     );
 
-    @GetExchange
+    // e.g.,
+    // https://mobile-clients-api.rewe.de/api/products?query=Kase&page=1&objectsPerPage=30
+    @GetExchange(REWE_CLIENT_API_BASE_URL + PRODUCT_SEARCH_PATH)
     ProductSearchResponse searchProducts(
-        URI uri, 
         @RequestParam("query") String product,
-        @RequestParam("page") String page, 
-        @RequestParam("objectsPerPage") String objectPerPage, 
+        @RequestParam(name = "page") String page, 
+        @RequestParam(name = "objectsPerPage") String objectsPerPage,
         @RequestHeader("rd-market-id") String marketId
     );
 }
